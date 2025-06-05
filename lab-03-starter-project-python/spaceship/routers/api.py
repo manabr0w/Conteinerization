480e660cf6eba8f2793814b8a226a90e86226bde
MaksFed <maksimfedorenko71@gmail.com>
2025-06-05 13:28:40 +0300

    created endpoint for multiplying matrix

--- lab-03-starter-project-python/spaceship/routers/api.py
+++ lab-03-starter-project-python/spaceship/routers/api.py
@@ -1,8 +1,19 @@
 from fastapi import APIRouter
-
+import numpy as np
 router = APIRouter()
 
 
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get("/matrix-mult")
+async def matrix_mult():
+    a = np.round(np.random.rand(10, 10),2)
+    b = np.round(np.random.rand(10, 10),2)
+    product = a.dot(b)
+    return {
+        "matrix_a": a.tolist(),
+        "matrix_b": b.tolist(),
+        "product": product.tolist(),
+    }
